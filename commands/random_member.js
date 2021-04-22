097995aa253d679428b523bbc4cea91b9c19c07d
Barthet Virgile <64908818+VClone-bot@users.noreply.github.com>
2021-04-22 19:10:01 +0200

    Working on a random command

---
+++ commands/random_member.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const { execute } = require('./ag_poll')
+
+module.exports = {
+    name: 'random_member',
+    aliases: ['rd_member', 'memberroll'],
+    description: 'tag un membre du discord aléatoire',
+    async execute(client, message, args) {
+
+        /** Get the list of every tag-able user in the current channel */
+        let members = message.channel.members;
+        console.log(members);
+    }
+}

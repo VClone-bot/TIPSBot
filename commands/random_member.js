9a2921ace92cbad0f4c62b32a8d7cf64d32e0c8d
Barthet Virgile <64908818+VClone-bot@users.noreply.github.com>
2021-04-22 19:34:29 +0200

    How are you today ?

--- commands/random_member.js
+++ commands/random_member.js
@@ -8,9 +8,7 @@ module.exports = {
     async execute(client, message, args) {
 
         /** Get the list of every tag-able user in the current channel */
-        const list = client.guilds.cache.get("442109249389199362");
-        list.members.cache.array().forEach(member => {
-            console.log(member.tag)
-        });
+        var user = message.guild.members.random();
+        console.log(user.user);
     }
 }
